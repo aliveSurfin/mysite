6a5c7267940c0768040c54365246fbde08558e39
alivesurfin <alivesurfin@gmail.com>
2022-09-24 12:38:24 +0100

    Basic react layout

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.scss";
+import Main from "./comps/Main/Main";
+import Footer from "./comps/Footer/Footer";
+import Header from "./comps/Header/Header";
+function App() {
+  return (<>
+  <Header></Header>
+  <Main></Main>
+  <Footer></Footer>
+  </>)
+}
+
+export default App;
